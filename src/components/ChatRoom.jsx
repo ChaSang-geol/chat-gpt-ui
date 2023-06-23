aed14a6c71f2d599c669f2cc223cecbb1ec518d5
ChaSang-geol <hunipapa@nate.com>
2023-06-24 02:08:07 +0900

    2030623

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,12 +1,12 @@
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 import { useState, useCallback, useEffect, useRef } from "react";
 import sendMessageService from "../service/sendmessage";
 import UserLoginSet from "./UserLoginSet";
-import ChatViewPrompt from "./ChatViewPrompt";
-import ChatViewAnswer from "./ChatViewAnswer";
-import PromptForm from "./PromptForm";
-import ChatHistory from "./ChatHistory";
-
+// import ChatViewPrompt from "./ChatViewPrompt";
+// import ChatViewAnswer from "./ChatViewAnswer";
+// import PromptForm from "./PromptForm";
+// import ChatHistory from "./ChatHistory";
+// import { handleSubmitApiCallback } from "../service/chatmessageutil";
 
 
 const ChatRoom = () => {
@@ -17,6 +17,7 @@ const ChatRoom = () => {
   const [messages, setMessages] = useState([]);
   const [count, setCount] = useState(0);
   const chatWindow = useRef(null);
+  const [asktime, setAsktime] = useState("")
 
   // useEffect(() => {
   //   function fetchCarList() {
@@ -25,47 +26,39 @@ const ChatRoom = () => {
   //   fetchCarList()
   // }, [])
 
-  // 새 메시지를 받으면 스크롤을 이동하는 함수
-  const moveScrollToReceiveMessage = useCallback(() => {
-    if (chatWindow.current) {
-      chatWindow.current.scrollTo({
-        top: chatWindow.current.scrollHeight,
-        behavior: "smooth",
-      });
-    }
-  }, []);
 
-  function makeMessage(prompt, userid) {
-    setCount((count) => count + 1);
-    console.log("Count : " + count);
-    var now = new Date(); // 현재시간
-    var nowTime =
-      now.getFullYear() +
-      "년" +
-      (now.getMonth() + 1) +
-      "월" +
-      now.getDate() +
-      "일" +
-      now.getHours() +
-      "시" +
-      now.getMinutes() +
-      "분" +
-      now.getSeconds() +
-      "초";
-    console.log("현재시간 : " + nowTime);
-    const dayjs = require("dayjs");
-    let varasktime = dayjs().format("h:mm A");
-    setResponse(sendMessageService.sendmessage(prompt, userid));
-    let varrestime = dayjs().format("h:mm A");
 
-    return {
-      id: count,
-      asktime: varasktime,
-      prompt: prompt,
-      restime: varrestime,
-      answer: response,
-    };
-  }
+  // function makeMessage(prompt, userid) {
+  //   setCount((count) => count + 1);
+  //   console.log("Count : " + count);
+  //   var now = new Date(); // 현재시간
+  //   var nowTime =
+  //     now.getFullYear() +
+  //     "년" +
+  //     (now.getMonth() + 1) +
+  //     "월" +
+  //     now.getDate() +
+  //     "일" +
+  //     now.getHours() +
+  //     "시" +
+  //     now.getMinutes() +
+  //     "분" +
+  //     now.getSeconds() +
+  //     "초";
+  //   console.log("현재시간 : " + nowTime);
+  //   const dayjs = require("dayjs");
+  //   let varasktime = dayjs().format("h:mm A");
+  //   setResponse(sendMessageService.sendmessage(prompt, userid));
+  //   let varrestime = dayjs().format("h:mm A");
+
+  //   return {
+  //     id: count,
+  //     asktime: varasktime,
+  //     prompt: prompt,
+  //     restime: varrestime,
+  //     answer: response,
+  //   };
+  // }
 
   const onChange = (e) => {
     setPrompt(e.target.value);
@@ -82,87 +75,78 @@ const ChatRoom = () => {
       }
       // alert("#1 : " + prompt);
 
-      setCount((count) => count + 1);
-      console.log("Count : " + count);
-      var now = new Date(); // 현재시간
-      var nowTime =
-        now.getFullYear() +
-        "년" +
-        (now.getMonth() + 1) +
-        "월" +
-        now.getDate() +
-        "일" +
-        now.getHours() +
-        "시" +
-        now.getMinutes() +
-        "분" +
-        now.getSeconds() +
-        "초";
-      console.log("현재시간 : " + nowTime);
+      // setCount((count) => count + 1);
+      // console.log("Count : " + count);
+      // var now = new Date(); // 현재시간
+      // var nowTime =
+      //   now.getFullYear() +
+      //   "년" +
+      //   (now.getMonth() + 1) +
+      //   "월" +
+      //   now.getDate() +
+      //   "일" +
+      //   now.getHours() +
+      //   "시" +
+      //   now.getMinutes() +
+      //   "분" +
+      //   now.getSeconds() +
+      //   "초";
+      // console.log("현재시간 : " + nowTime);
       const dayjs = require("dayjs");
-      let varasktime = dayjs().format("h:mm A");
+      // let varasktime = dayjs().format("h:mm A");
+      setAsktime(dayjs().format("h:mm A"));
       // 질문 결과 받기
       // let test_data = await sendMessageService.handleSubmitApi(prompt, userid);
 
-      const handleSubmitApiCallback = (response) => {
-        setResponse(response);
-        let varrestime = dayjs().format("h:mm A");
-        if (response) {
-          // eslint-disable-next-line react-hooks/exhaustive-deps
-          newMessage = {
-            id: count,
-            asktime: varasktime,
-            prompt: prompt,
-            restime: response?.restime,
-            answer: response?.answer,
-          }; // prompt 를 전달하여 API로 부터 결과를 응답 받아 입력
-          console.log("newMessage : ", newMessage);
-          // alert("#2 : " + toString(newMessage));
-          addMessage(newMessage);
-          // setMessages((messages) => [...messages, newMessage]);
-        }
-        // console.log(messages);
-        moveScrollToReceiveMessage(); // 메시지 응답후 창을 스크롤
-        // sendMessageService.sendmessage(prompt, userid);
-        // alert("#3 : " + messages);
-      }
-
       sendMessageService.handleSubmitApi({
-        prompt, 
-        userid, 
-        callback : handleSubmitApiCallback
+        prompt,
+        userid,
+        callback: handleSubmitApiCallback
       });
 
-     
+      moveScrollToReceiveMessage(); // 메시지 응답후 창을 스크롤
 
-
-      
     },
     [prompt, response]
   );
 
-  // useEffect(() => {
-  //   if (response) {
-  //     console.log("response", response);
-  //     // setResponse(response);
-  //     // setNewMessage({
-  //     //   id: newMessage.id,
-  //     //   asktime: newMessage.asktime,
-  //     //   prompt: newMessage.prompt,
-  //     //   restime: response.restime,
-  //     //   answer: response.answer,
-  //     // });
-  //   }
-  //   console.log("response set");
-  // }, [response, newMessage]);
+  const handleSubmitApiCallback = (response) => {
+    // setResponse(response);
+    setCount((count) => count + 1);
+    // let varrestime = dayjs().format("h:mm A");
+    if (response) {
+      newMessage = {
+        id: count,
+        asktime: asktime,
+        prompt: prompt,
+        restime: response?.restime,
+        answer: response?.answer,
+      }; // prompt 를 전달하여 API로 부터 결과를 응답 받아 입력
+      console.log("newMessage : ", newMessage);
 
-  const addMessage = useCallback(
-    (newMessage) => {
-      setMessages((messages) => [...messages, newMessage]);
-      console.log("messages : ", messages);
-    },
+      addMessage(newMessage);
+
+    }
+
+
+  };
+
+  const addMessage = useCallback((newMessage) => {
+    setMessages((messages) => [...messages, newMessage]);
+    console.log("messages : ", messages);
+  },
     [messages]
   );
+
+  // 새 메시지를 받으면 스크롤을 이동하는 함수
+  const moveScrollToReceiveMessage = useCallback(() => {
+    if (chatWindow.current) {
+      chatWindow.current.scrollTo({
+        top: chatWindow.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, []);
 
   return (
     <>
