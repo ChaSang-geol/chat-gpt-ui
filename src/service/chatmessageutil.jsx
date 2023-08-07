85584b4d9fdd9550185b3e52b8565e8e0f2a9683
ChaSang-geol <hunipapa@nate.com>
2023-08-07 14:44:05 +0900

    20230807

--- src/service/chatmessageutil.jsx
+++ src/service/chatmessageutil.jsx
@@ -12,41 +12,41 @@ const chatWindow = useRef(null);
 // handleSubmitApiCallback
 
 export const handleSubmitApiCallback = (response) => {
-    // setResponse(response);
-    setCount((count) => count + 1);
-    // let varrestime = dayjs().format("h:mm A");
-    if (response) {
-        newMessage = {
-            id: count,
-            asktime: asktime,
-            prompt: prompt,
-            restime: response?.restime,
-            answer: response?.answer,
-        }; // prompt 를 전달하여 API로 부터 결과를 응답 받아 입력
-        console.log("newMessage : ", newMessage);
+  // setResponse(response);
+  setCount((count) => count + 1);
+  // let varrestime = dayjs().format("h:mm A");
+  if (response) {
+    newMessage = {
+      id: count,
+      asktime: asktime,
+      prompt: prompt,
+      restime: response?.restime,
+      answer: response?.answer,
+    }; // prompt 를 전달하여 API로 부터 결과를 응답 받아 입력
+    console.log("newMessage : ", newMessage);
 
-        addMessage(newMessage);
+    addMessage(newMessage);
 
-    }
+  }
 
-    moveScrollToReceiveMessage(); // 메시지 응답후 창을 스크롤
+  moveScrollToReceiveMessage(); // 메시지 응답후 창을 스크롤
 
 };
 
 export const addMessage = useCallback((newMessage) => {
-    setMessages((messages) => [...messages, newMessage]);
-    console.log("messages : ", messages);
+  setMessages((messages) => [...messages, newMessage]);
+  console.log("messages : ", messages);
 },
-    [messages]
+  [messages]
 );
 
 // 새 메시지를 받으면 스크롤을 이동하는 함수
 export const moveScrollToReceiveMessage = useCallback(() => {
-    if (chatWindow.current) {
-        chatWindow.current.scrollTo({
-            top: chatWindow.current.scrollHeight,
-            behavior: "smooth",
-        });
-    }
+  if (chatWindow.current) {
+    chatWindow.current.scrollTo({
+      top: chatWindow.current.scrollHeight,
+      behavior: "smooth",
+    });
+  }
 }, []);
 // }
