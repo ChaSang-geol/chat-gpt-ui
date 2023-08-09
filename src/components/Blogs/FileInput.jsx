c1670420bb947a86bd873700c390b1613c0d0560
ChaSang-geol <hunipapa@nate.com>
2023-08-09 23:03:33 +0900

    20230809

--- src/components/Blogs/FileInput.jsx
+++ src/components/Blogs/FileInput.jsx
@@ -1,12 +1,12 @@
 import { useState, useCallback, useEffect, useRef } from "react";
 import axios from "axios";
-import * as fs from 'node:fs/promises';
+// import * as fs from 'node:fs';
 
 const FileInput = () => {
 
   const handleSubmit = async() => {
 
-    var fs = require('fs');
+    // var fs = require('fs');
     
     let headersList = {
      "Authorization": "Bearer predefinedMyApiKey" 
@@ -14,7 +14,8 @@ const FileInput = () => {
     let uploadFileName = document.getElementById("uploadFile").value;
     let formdata = new FormData();
     // formdata.append("file", fs.createReadStream("c:\workspace\chat-gpt-ui\public\logo512.png"));
-    formdata.append("file", fs.createReadStream(uploadFileName));
+    // formdata.append("file", fs.createReadStream(uploadFileName));
+    formdata.append("file", uploadFileName);
     
     let bodyContent =  formdata;
     
@@ -32,7 +33,7 @@ const FileInput = () => {
 
 return (
   <>
-    <form>
+    <form onSubmit={handleSubmit}>
       <div class="custom-file">
         <input type="file" class="custom-file-input" id="uploadFile" />
         <label class="custom-file-label" for="uploadFile">Choose file</label>
